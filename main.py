61b1445fe34fd0dee3d5737ba6f1d73f89519320
Soebb <87156166+Soebb@users.noreply.github.com>
2022-03-09 09:40:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -17,33 +17,6 @@ Bot = Client(
     bot_token=BOT_TOKEN
 )
 
-# {ee}
-ee = "م"
-s="masum"
-e=3
-fa="آپارتمان بی گناهان"
-x='\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"\"-\"'
-text=f"سریال {s} {fa} قسمت {e} با زیرنویس فارسی" \
-     f"\nقسمت {ee} سریال {fa} {s} با زیرنویس چسبیده رایگان" \
-     f"\nقسمت {e} سریال {fa} - {s} با زیرنویس فارسی چسبیده دی ال مکوین" \
-     f"\nتماشای قسمت بعدی در کانال تلگرام ما :" \
-     f"\nhttps://t.me/joinchat/Rguc8ahmI2pnKElU" \
-     f"\n,سریال {fa}" \
-     f"\n,{fa}" \
-     f"\n,{fa}{e}" \
-     f"\n,سریال {fa}{e}" \
-     f"\n-------------------------" \
-     f"\nزیرنویس چسبیده قسمت {e} سریال ترکی {fa} قسمت 54 {s}" \
-     f"\nقسمت {e} سریال {fa} با زیرنویس چسبیده قسمت {ee} 54 {s}" \
-     f"\nسریال {fa} {e} {s} قسمت {ee} با زینویس چسبیده" \
-     f"\nجهت دانلود تماشای کامل این قسمت کانال تلگرام دی ال مکوین شوید :" \
-     f"\nhttps://t.me/joinchat/Rguc8ahmI2pnKElU" \
-     f"\n,سریال {fa}" \
-     f"\n,{fa}" \
-     f"\n,{fa}{e}" \
-     f",سریال {fa}{e}"
-
-
 
 @Bot.on_message(filters.private & filters.text)
 async def webtopdf(_, m):
@@ -59,10 +32,14 @@ async def webtopdf(_, m):
     }
     #os.system(f'yt-dlp -vU --geo-bypass --no-check-certificate -o "v.mp4" "{url}"')
     with YoutubeDL(opts) as ytdl:
-        ytdl.download([url])
-    #    ytdl.extract_info(url, download=True)
+        #ytdl.download([url])
+        info = ytdl.extract_info(url, download=False)
     time.sleep(30)
-    M=await m.reply_document(name)
+    try:
+        await m.reply(info)
+    except Exception as e:
+        await m.reply(e)
+    #await m.reply_document(name)
     #await M.edit(M.document.file_name)
     os.remove(name)
 
